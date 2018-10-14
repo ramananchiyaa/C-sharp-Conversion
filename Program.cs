ee00798ddeddef107a57a89f23f6d044013b67f2
sriramananm <42888174+sriramananm@users.noreply.github.com>
2018-10-14 09:49:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convert_Image_to_Pdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();
+            f.OpenPdf(@"D:\Integra training\fileConversion\scansmpl.pdf");
+
+            if (f.PageCount > 0)
+            {
+                f.ImageOptions.Dpi = 200;
+                f.ImageOptions.ImageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+
+                for (int page = 1; page <= f.PageCount; page++)
+                {
+                    f.ToImage(@"D:\Integra training\fileConversion\images\" + "page" + page + ".jpg", page);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
